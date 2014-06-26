f99e633e55f2c98dfc30d0b37e19f3f5a4b11c01
Bryce <lichao0407@gmail.com>
2014-06-26 18:39:07 +0800

    share

--- app/src/main/java/com/cyeam/cyeamdroid/app/PostActivity.java
+++ app/src/main/java/com/cyeam/cyeamdroid/app/PostActivity.java
@@ -1,19 +1,27 @@
 package com.cyeam.cyeamdroid.app;
 
-import android.app.ActionBar;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cyeam.cyeamdroid.http.CyeamHttp;
 import com.cyeam.cyeamdroid.model.Blog;
 import com.loopj.android.http.TextHttpResponseHandler;
 
 import org.apache.http.Header;
+
+import java.util.List;
 
 /**
  * Created by bryce on 14-6-25.
@@ -24,6 +32,7 @@ public class PostActivity extends Activity {
     private TextView date;
     private ImageView figure;
     private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /*显示App icon左侧的back键*/
@@ -31,12 +40,12 @@ public class PostActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
-        Blog blog = (Blog)getIntent().getSerializableExtra(Blog.BLOG_SER);
+        Blog blog = (Blog) getIntent().getSerializableExtra(Blog.BLOG_SER);
         this.blog = blog;
-        title = (TextView)findViewById(R.id.post_title);
-        date = (TextView)findViewById(R.id.post_date);
-        figure = (ImageView)findViewById(R.id.post_figure);
-        webView = (WebView)findViewById(R.id.post);
+        title = (TextView) findViewById(R.id.post_title);
+        date = (TextView) findViewById(R.id.post_date);
+        figure = (ImageView) findViewById(R.id.post_figure);
+        webView = (WebView) findViewById(R.id.post);
     }
 
     @Override
@@ -60,7 +69,14 @@ public class PostActivity extends Activity {
             });
         }
 
-        webView.loadDataWithBaseURL("", blog.getDescription(), "text/html", "UTF-8","");
+        webView.loadDataWithBaseURL("", blog.getDescription(), "text/html", "UTF-8", "");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.post, menu);
+        return true;
     }
 
     @Override
@@ -68,8 +84,24 @@ public class PostActivity extends Activity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
+                break;
+            case R.id.action_share:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TITLE, blog.getTitle());
+                intent.putExtra(Intent.EXTRA_SUBJECT, blog.getLink());
+                intent.putExtra(Intent.EXTRA_TEXT, blog.getInfo() + " " + blog.getLink());
+
+                Intent chooserIntent = Intent.createChooser(intent, "Select app to share");
+                try {
+                    startActivity(chooserIntent);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(this, "Can't find share component to share", Toast.LENGTH_SHORT).show();
+                }
+                break;
             default:
                 return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
     }
 }
